94fba92818367204e5e0e04d7e510bd1436ebb94
neB <benjamin.sullca1103@gmail.com>
2025-09-30 14:52:53 -0500

    feat: crud de usuarios

---
+++ app/src/main/java/com/example/datossinmvvm/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.datossinmvvm
+
+import ScreenUser
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.datossinmvvm.ui.theme.DatosSinMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DatosSinMVVMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ScreenUser(
+                        modifier = Modifier.padding(innerPadding)
+                    ) //
+                }
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/datossinmvvm/UserDao.kt
@@ -0,0 +1,18 @@
+package com.example.datossinmvvm
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM User")
+    suspend fun getAll(): List<User>
+
+    @Insert
+    suspend fun insert(user: User)
+
+}
+
